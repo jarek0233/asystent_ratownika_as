7da82115360a7668ad2b173c6c748db61099c34d
Dawid Markiewka <dawid.markiewka@live.at>
2024-06-11 17:52:29 +0200

    1.2

--- app/src/main/java/com/example/aystent_ratownika/MainActivity.java
+++ app/src/main/java/com/example/aystent_ratownika/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.aystent_ratownika;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
+import android.widget.EditText;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,7 +13,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText emailEditText;
+    EditText passwordEditText;
+    button loginButton;
+    Button registerButton;
+    Button exitButton;
 
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +30,31 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
+
         });
+
+        emailEditText = findViewById(R.id.editTextTextEmailAddress);
+        passwordEditText = findViewById(R.id.editTextTextPassword);
+        loginButton = findViewById(R.id.button2);
+        registerButton = findViewById(R.id.button3);
+        exitButton = findViewById(R.id.button4);
+
+        loginButton.setOnClickListener(v -> {
+            String email = emailEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+
+            if (validateLogin(email, password)) {
+                Toast.makeText(MainActivity.this, "Zalogowano pomyślnie", Toast.LENGTH_SHORT).show();
+
+
+            } else {
+                Toast.makeText(MainActivity.this, "Niepoprawne dane logowania", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+
     }
 }
